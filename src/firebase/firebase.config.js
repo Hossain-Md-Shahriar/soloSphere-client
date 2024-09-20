3a04429ba9aa8f2774aea915ef1285def0b66cfe
Hossain-Md-Shahriar <hasibshahriar1@gmail.com>
2024-09-20 10:52:48 +0600

    Authentication done

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAv2i-kB-rk7T_7cEYre09RbSfGgGfhz6E",
+  authDomain: "solosphere-a2a0e.firebaseapp.com",
+  projectId: "solosphere-a2a0e",
+  storageBucket: "solosphere-a2a0e.appspot.com",
+  messagingSenderId: "959483371328",
+  appId: "1:959483371328:web:c22b71a12c0b2a873e24a5"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
